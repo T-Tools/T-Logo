529bb0d16fc85ac2a34eb1f3f88244191d7f66e9
ypn007 <55645320+ypn007@users.noreply.github.com>
2019-12-29 12:17:49 +0000

    Add files via upload

---
+++ logo.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# START
+
+
+#These Are Codes
+RED='\033[0;31m'
+BLACK='\033[0;30m'
+DG='\033[1;31m'
+LIGHT RED='\033[1;31m'
+GR='\033[0;32m'
+LIGHTGREEN='\033[1;32m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+LIGHT BLUE='\033[1;34m'
+PURPLE='\033[0;35m'
+LIGHT PURPLE='\033[1;35m'
+CYAN='\033[0;36m'
+LIGHT CYAN='\033[0;36m'
+GRAY='\033[0;37m'
+LIGHT GRAY='\033[0;37m'
+RED='\033[0;31m'
+YO='\033[0;33m'
+BLU='\033[0;34m'
+#
+clear
+echo ${GR} ""
+figlet Sql Vulnerability Scanner
+echo ""                                     
+echo "                        ${YO}Let's Make Your Exploitation And Have Fun"
+echo "" 
+echo " ${RED}                       ==[ Note:This Tool Is For Beginner "
+echo "  ${GR}                      ==[ Note:Don't Use This Tool "
+echo "  ${BLU}                      ==[ Note:To Harm Something "
+echo "  ${GRAY}                      ==[ Note:Hack Everything,Harm Nothing"
+echo "  ${GR}                      ==[ Tools Name : Termux Logo Changer"
+echo "  ${GR}                      ==[ Coded by : Young Professional Terminal(YPT)"
+echo "   ${RED}                     ==[ Version : 1.0.0"
+echo "  ${GR}                      ==[ Program Type :Bash"
+echo "  ${GR}                      ==[ I Am Myanmer Young Hacker😉😉😉"
+echo "  ${GR}                      ==[ www.github.com/T-Tools"                 
+echo "  ${GR}                      ==[ https://www.facebook.com/101466121360286"
+echo ""
---
+++ t-logo.sh
@@ -0,0 +1,49 @@
+#!/bin/bash 
+#This Is Color
+RED='\033[0;31m'
+GR='\033[0;32m'
+#End Of Colour
+clear
+#Change Directory
+cd $PREFIX/etc
+#Remove Old files
+rm bash.bashrc
+rm motd
+#Name
+sh logo.sh
+echo "${GR}What Is Your Name ?"
+read name
+# Open file descriptor (fd) 3 for read/write on a bash file.
+ exec 3<> bash.bashrc
+ # Let's print some text to fd 3
+echo " PS1='\033[1;32m
+\a┏\a━━We━━━Are━━━━━Myanmar━┓\033[1;32m
+\a┏\a━\a┃ Young\a  Professional \aTermux ┃--┃ \@ ┃---| \d ┃
+\a\a┃\a \a\033[1;32m┗━$name━┛\033[1;36m
+\a┗\a┳\a━\a📂\033[1;32m\w\a📂
+┗ 🇲🇲 \# 👨‍🔧 -❕\W 🔋🔌🖱️🔰✏️ '
+
+
+
+figlet $name | lolcat -a -d 20
+
+shopt -s autocd
+shopt -s cdspell
+shopt -s checkhash
+shopt -s checkwinsize
+shopt -s compat31
+shopt -s compat32
+shopt -s compat40
+shopt -s compat41
+shopt -s no_empty_cmd_completion
+shopt -s histverify
+shopt -s histappend
+shopt -s dirspell
+shopt -s direxpand
+shopt -s compat43
+shopt -s compat32
+shopt -s lithist ">&3
+
+ # Close fd 3 
+exec 3>&-
+echo " ${RED}Termux Logo Was Changed Go To New Session To See The New"
